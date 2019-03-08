e6665892d2139e6bcb37a79b13d524aca19ea2ba
InsertnameEx3 <Marten@homma-clan.nl>
2019-03-08 22:48:40 +0100

    Added insert

---
+++ dotNetTest/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using dotNetTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dotNetTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        User users = new User();
+        Noun nouns = new Noun();
+        Verb verbs = new Verb();
+        Answer answers = new Answer();
+        Question questions = new Question();
+
+
+
+        public ActionResult Index()
+        {
+
+            //Response.Write(test.Get("SELECT * FROM [user]", 1));
+            ViewData["Users"] = users.Get("SELECT * FROM [user]", 1);
+            ViewData["Nouns"] = nouns.Get("SELECT * FROM [noun]", 1);
+            ViewData["Verbs"] = verbs.Get("SELECT * FROM [verb]", 1);
+            ViewData["Questions"] = questions.Get("SELECT * FROM [question]", 2);
+            ViewData["Answers"] = answers.Get("SELECT * FROM [answer]", 1);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ChartArrayBasic()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+    }
+}
---
+++ dotNetTest/Models/Model.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace dotNetTest.Models
+{
+    public class Model
+    {
+        public List<string> Get(string query, int column)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=prototype; Integrated Security=SSPI;";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            List<string> Output = new List<string> { };
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+
+
+            string sql = query;
+
+            command = new SqlCommand(sql, cnn);
+
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+
+                Output.Add(dataReader.GetValue(column).ToString());
+
+            }
+
+            dataReader.Close();
+            command.Dispose();
+            cnn.Close();
+
+            return Output;
+        }
+        public void Insert(string variable, string table, string column)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            connetionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=prototype; Integrated Security=SSPI;";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+         
+            SqlCommand command;
+            SqlDataReader dataReader;
+
+     
+            
+            string sql =
+                "BEGIN IF NOT EXISTS(SELECT *  FROM ["+table+"] WHERE "+column+" = '"+variable+"') BEGIN INSERT INTO "+table+"("+column+") VALUES('"+variable+"') END END";
+      
+
+
+            command = new SqlCommand(sql, cnn);
+            adapter.InsertCommand = command;
+            adapter.InsertCommand.ExecuteNonQuery();
+
+            
+            command.Dispose();
+            cnn.Close();
+        }
+    }
+
+
+
+
+
+
+
+
+}
